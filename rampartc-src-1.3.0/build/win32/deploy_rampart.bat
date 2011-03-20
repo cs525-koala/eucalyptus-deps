6f71cf2cabfc88c23dd3f3d42e0bc8479613ff54
Will Dietz <w@wdtz.org>
2011-03-19 22:28:34 -0500

    Extract dependencies.

---
+++ rampartc-src-1.3.0/build/win32/deploy_rampart.bat
@@ -0,0 +1,20 @@
+@echo off
+echo -------------------------------------------------------------------------
+echo deploying rampart...
+echo -------------------------------------------------------------------------
+
+xcopy .\include %AXIS2C_HOME%\include /E /I /Y /S
+xcopy .\lib %AXIS2C_HOME%\lib /E /I /Y /S
+xcopy .\modules %AXIS2C_HOME%\modules /E /I /Y /S
+xcopy .\samples %AXIS2C_HOME%\samples /E /I /Y /S
+xcopy .\services %AXIS2C_HOME%\services /E /I /Y /S
+copy .\samples\src\rampartc\data\server_axis2.xml %AXIS2C_HOME%\axis2.xml
+
+cd .\samples\src\rampartc\client
+deploy_client_repo.bat
+cd ..\..\..\..\
+
+echo -------------------------------------------------------------------------
+echo Rampart deployed
+echo -------------------------------------------------------------------------
+@echo on
